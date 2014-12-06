8b48672ebd598ba285f07c29fc2d2df78799adf9
Alex Kerney <abk@mac.com>
2014-12-06 16:22:44 -0500

    I can create and edit everything but a geometry collection.

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,7 +2,7 @@ from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView as SQLAModelView
 
 from .. import db
-from ..models import POI
+from ..models import POI, Line, Polygon, MultiPoint, MultiLine, MultiPolygon, Collection
 
 from . import form, typefmt
 
@@ -17,3 +17,9 @@ view = POIView(POI, db.session)
 
 admin = Admin(name="flask-admin.contrib.geoa testing")
 admin.add_view(view)
+admin.add_view(ModelView(Line, db.session))
+admin.add_view(ModelView(Polygon, db.session))
+admin.add_view(ModelView(MultiLine, db.session))
+admin.add_view(ModelView(MultiPolygon, db.session))
+admin.add_view(ModelView(MultiPoint, db.session))
+admin.add_view(ModelView(Collection, db.session))
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from geoalchemy2.shape import to_shape
 from sqlalchemy import func
 
 class POI(db.Model):
+	# Maine around Baxter
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(40))
 	point = db.Column(Geometry("POINT", srid=926919))
@@ -13,3 +14,39 @@ class POI(db.Model):
 	#
 	#def point_shape_web(self):
 	#	return to_shape(db.session.scalar(self.point.ST_Transform(4326)))
+
+class Line(db.Model):
+	# New Hampshire
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	line = db.Column(Geometry("LINESTRING", srid=32010))
+
+class Polygon(db.Model):
+	# Western Maine
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	polygon = db.Column(Geometry("POLYGON", srid=2802))
+
+class MultiPoint(db.Model):
+	# Maine around Baxter
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	multipoint = db.Column(Geometry("MULTIPOINT", srid=926919))
+
+class MultiLine(db.Model):
+	# New Hampshire
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	multiline = db.Column(Geometry("MULTILINESTRING", srid=32010))
+
+class MultiPolygon(db.Model):
+	# Western Maine
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	multipolygon = db.Column(Geometry("MULTIPOLYGON", srid=2802))
+
+class Collection(db.Model):
+	# Web
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	collection = db.Column(Geometry("GEOMETRYCOLLECTION", srid=4326))
